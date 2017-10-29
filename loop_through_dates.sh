94b2a3e7af364c35dc14831ddeb7d97e80633c39
Jessica Yung <jessicayungyc@gmail.com>
2017-10-29 07:29:02 +0000

    feat: loop through dates works - manually specify year, symbol and range of dates (e.g. 1-3 gives first three days in 2016

--- loop_through_dates.sh
+++ loop_through_dates.sh
@@ -1,18 +1,17 @@
 year=2016
 file="dates${year}.txt"
-echo $file
-for symbol in "AAPL"
+for symbol in "\$AAPL"
 do
 	for i in `seq 1 3`
 	do
         #echo $i
         j=$(($i+1))
-        echo $j
-	    start_date= sed "${i}q;d" $file
-		end_date= sed "${j}q;d" $file
+        #echo $j
+	    start_date=`sed "${i}q;d" $file`
+        echo "start_date added"
+		end_date=`sed "${j}q;d" $file`
         filename="$symbol${year}_$i.csv"
-        echo $start_date $end_date $filename
-		# python Exporter.py --querysearch $symbol --since $start_date --until $end_date --output $filename
+		python Exporter.py --querysearch $symbol --since $start_date --until $end_date --output $filename
 	done
 done
 
